71c1011ffd39153cfa5c17fd5d64d0ad62fdc37a
kimi <hakimihamzan20@gmail.com>
2022-04-05 23:18:42 +0800

    Edit: revamp HeaderSocial component

--- src/components/header/HeaderSocial.jsx
+++ src/components/header/HeaderSocial.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { BsLinkedin } from 'react-icons/bs'
 import { FaGithub } from 'react-icons/fa'
 import { FiDribbble } from 'react-icons/fi'
+import styles from './HeaderSocial.module.css'
 
 const HeaderSocial = () => {
     return (
-        <div className='header__socials'>
+        <div className={styles.header__socials}>
             <a href="https://www.linkedin.com/in/hakimihamzan/" rel="noreferrer" target='_blank'><BsLinkedin /></a>
             <a href="https://github.com/hakimihamzan" rel="noreferrer" target='_blank'><FaGithub /></a>
-            <a href="https://linkedin.com" rel="noreferrer" target='_blank'><FiDribbble /></a>
         </div>
     )
 }
