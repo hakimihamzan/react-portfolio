d5bc90e019b519704a72d8cfabc98af68e376fd5
kimi <hakimihamzan20@gmail.com>
2022-04-07 21:00:43 +0800

    Paused: Each portfolio not yet finished rendering

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,11 +1,54 @@
 import React from 'react'
-import './Portfolio.css'
+import styles from './Portfolio.module.css'
+import port from '../../assets/portfolio1.jpg'
+import Modal from '../modal/Modal'
 
 const Portfolio = () => {
     return (
         <section id='portfolio'>
             <h5>My Portfolio</h5>
             <h2>Projects & Showcase</h2>
+
+            <div className={styles.portfolio__container}>
+                <div className={styles.project}>
+                    <img className={styles.portfolio__image} src={port} alt="" />
+                    <h4>Bugs Tracking Web Application</h4>
+                    <p className='text-light'>Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
+                    <Modal name='bugstracking' />
+                </div>
+                <div className={styles.project}>
+                    <img className={styles.portfolio__image} src={port} alt="" />
+                    <h4>Landing Page for Android app</h4>
+                    <p className='text-light'>Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
+                    <Modal name='fitforchange' />
+                </div>
+                <div className={styles.project}>
+                    <img className={styles.portfolio__image} src={port} alt="" />
+                    <h4>Bugs Tracking Web Application</h4>
+                    <p className='text-light'>Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
+                    <Modal name='codingglossary' />
+                </div>
+                <div className={styles.project}>
+                    <img className={styles.portfolio__image} src={port} alt="" />
+                    <h4>Bugs Tracking Web Application</h4>
+                    <p className='text-light'>Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
+                    <Modal name='codingglossary' />
+                </div>
+                <div className={styles.project}>
+                    <img className={styles.portfolio__image} src={port} alt="" />
+                    <h4>Bugs Tracking Web Application</h4>
+                    <p className='text-light'>Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
+                    <Modal name='codingglossary' />
+                </div>
+                <div className={styles.project}>
+                    <img className={styles.portfolio__image} src={port} alt="" />
+                    <h4>Bugs Tracking Web Application</h4>
+                    <p className='text-light'>Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
+                    <Modal name='codingglossary' />
+                </div>
+            </div>
+
+
         </section>
     )
 }
