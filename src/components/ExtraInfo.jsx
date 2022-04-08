ed3097512f58032f73e9ed3df6025be58027dbf3
kimi <hakimihamzan20@gmail.com>
2022-04-08 22:14:37 +0800

    Update: Project  data is dynamic

--- src/components/ExtraInfo.jsx
+++ src/components/ExtraInfo.jsx
@@ -1,66 +1,12 @@
 import React from 'react'
 import styles from './ExtraInfo.module.css'
 import { FaGithub, FaEye } from 'react-icons/fa'
+import data from '../data'
+
+
 
 function ExtraInfo({ handleClose, name }) {
-    const data = {
-        fitforchange: {
-            title: 'FIT FOR CHANGE',
-            url: 'https://fitforchange.xyz',
-            heading: 'Full static, responsive landing page for an upcoming Android application.',
-            date: 'APR2021',
-            notePoint: [
-                'Built with HTML, CSS, Bootstrap and Firebase.',
-                'Firebase (Firestore) is used as a database for storing name/email details of interested users.',
-            ],
-            techStack: "HTML, CSS, Bootstrap, Firebase",
-            designInspired: []
-        },
-
-        codingglossary: {
-            title: 'CODING GLOSSARY',
-            url: 'https://hakimihamzan.github.io/glossary/',
-            heading: 'Search engine for computer science related words. (Single Page Application)',
-            date: 'AUG2021',
-            notePoint: [
-                'Built with HTML, CSS, Bootstrap and JavaScript',
-                'JavaScript is used to update the page based on user’s interaction',
-            ],
-            techStack: "HTML, CSS, Bootstrap, JavaScript",
-            designInspired: []
-        },
-
-        bugstracking: {
-            title: 'BUGSTRACKER',
-            url: 'https://bugstracker.xyz',
-            codeUrl: 'https://github.com/hakimihamzan/bug-tracker',
-            heading: 'Full stack web application for submitting software defects encountered by users.',
-            date: 'SEPT2021',
-            notePoint: [
-                'Built with JavaScript, HTML, CSS, Firebase and GitHub API.',
-                'Firebase (Firestore) is used as a database for storing details of each bug.',
-                'Implemented Google Authentication & Authorization for separation of roles.',
-                'Implemented Demo Account for Developer and Manager'
-            ],
-            techStack: "HTML, CSS, JavaScript, Firebase, GitHub API",
-            designInspired: [
-                {
-                    url: 'https://coderthemes.com/hyper/creative/layouts-vertical.html',
-                    creator: 'CoderTheme',
-                    title: 'Hyper-V Bootstrap Framework'
-                },
-                {
-                    url: 'https://dribbble.com/shots/6398590-DoneDone-2-Mailbox-Home?showSimilarShots=true&_=1630390033997#',
-                    creator: 'PixelMatters',
-                    title: 'DoneDone 2'
-                },
-            ]
-        },
-    }
-
     const currentProject = { ...data[name] }
-
-    console.log(currentProject.designInspired.length)
 
     return (
         <>
@@ -71,7 +17,7 @@ function ExtraInfo({ handleClose, name }) {
                         <button><a href={currentProject.codeUrl} rel="noreferrer" target='_blank'> <FaGithub />CODE</a></button>
                         <button><a href={currentProject.url} rel="noreferrer" target='_blank'><FaEye />LIVE</a></button>
                     </div>
-                    <img src="https://images.pexels.com/photos/103123/pexels-photo-103123.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                    <img src={currentProject.snap} alt="project" />
                 </div>
                 <div className={styles.extraInfo}>
                     <h2>{currentProject.title} <small className='text-light' style={{ fontSize: '0.75rem' }}>({currentProject.date})</small></h2>
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,53 +2,32 @@ import React from 'react'
 import styles from './Portfolio.module.css'
 import port from '../../assets/portfolio1.jpg'
 import Modal from '../modal/Modal'
+import data from '../../data'
 
 const Portfolio = () => {
+    const projectNames = Object.keys(data);
     return (
         <section id='portfolio'>
             <h5>My Portfolio</h5>
             <h2>Projects & Showcase</h2>
 
             <div className={styles.portfolio__container}>
+                {projectNames.map((name, index) =>
+                (
+                    <div className={styles.project} key={index}>
+                        <img className={styles.portfolio__image} src={data[name].snap} alt="" />
+                        <h4>{data[name].title}</h4>
+                        <p className='text-light'>{data[name].heading.substring(0, 60)}...</p>
+                        <Modal name={name} />
+                    </div>
+                ))}
                 <div className={styles.project}>
                     <img className={styles.portfolio__image} src={port} alt="" />
-                    <h4>Bugs Tracking Web Application</h4>
+                    <h4>Lorem ipsum dolor sit amet</h4>
                     <p className='text-light'>Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
-                    <Modal name='bugstracking' />
-                </div>
-                <div className={styles.project}>
-                    <img className={styles.portfolio__image} src={port} alt="" />
-                    <h4>Landing Page for Android app</h4>
-                    <p className='text-light'>Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
-                    <Modal name='fitforchange' />
-                </div>
-                <div className={styles.project}>
-                    <img className={styles.portfolio__image} src={port} alt="" />
-                    <h4>Bugs Tracking Web Application</h4>
-                    <p className='text-light'>Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
-                    <Modal name='codingglossary' />
-                </div>
-                <div className={styles.project}>
-                    <img className={styles.portfolio__image} src={port} alt="" />
-                    <h4>Bugs Tracking Web Application</h4>
-                    <p className='text-light'>Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
-                    <Modal name='codingglossary' />
-                </div>
-                <div className={styles.project}>
-                    <img className={styles.portfolio__image} src={port} alt="" />
-                    <h4>Bugs Tracking Web Application</h4>
-                    <p className='text-light'>Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
-                    <Modal name='codingglossary' />
-                </div>
-                <div className={styles.project}>
-                    <img className={styles.portfolio__image} src={port} alt="" />
-                    <h4>Bugs Tracking Web Application</h4>
-                    <p className='text-light'>Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
-                    <Modal name='codingglossary' />
+                    <Modal name='' />
                 </div>
             </div>
-
-
         </section>
     )
 }
