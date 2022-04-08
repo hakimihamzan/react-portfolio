3c095de19625d07f7b4eebb3865dd864caf1e97c
kimi <hakimihamzan20@gmail.com>
2022-04-08 12:53:58 +0800

    Fix: bugs in small phone device overflow

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -6,10 +6,10 @@ import ExtraInfo from '../ExtraInfo';
 import { Fade } from '@mui/material';
 
 const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
+    position: 'fixed',
+    // top: '50%',
+    // left: '50%',
+    // transform: 'translate(-50%, -50%)',
     width: '100vw',
     height: '100vh',
     bgcolor: 'var(--color-bg)',
