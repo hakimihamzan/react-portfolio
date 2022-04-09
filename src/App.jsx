3337cc7549d49a32fad4acceec6f91fe6918600e
kimi <hakimihamzan20@gmail.com>
2022-04-09 18:59:04 +0800

    Update: images compressed to improve performance

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
             <Experience />
             <Portfolio />
             <Contact />
-            {/* <About /> */}
+            <About />
             {/* <Footer /> */}
         </>
     )
